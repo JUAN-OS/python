51b6b8bfddde2b629bb48597988c9590685742a5
JUAN-OS <77461042+JUAN-OS@users.noreply.github.com>
2021-01-16 16:06:24 -0500

    Add files via upload

---
+++ ecmascript/ES7/index.js
@@ -0,0 +1,14 @@
+let numbers = [1,2,3,7,8];
+
+if (numbers.includes(9)){
+    console.log('SI SE ENCUENTRA EL VALOR 7')
+}else{
+    console.log('NO SE ENCUENTRA')
+}
+
+// Programa de Exponente
+let base = 4;
+let exponet = 3;
+let result = base ** exponet;
+
+console.log(result);
